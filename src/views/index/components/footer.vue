6c494658e88a9ee98c782c3d8b456529272ec11d
ivwsjc <115985110+ivwsjc@users.noreply.github.com>
2023-04-09 23:24:18 +0800

    h5页脚

--- src/views/index/components/footer.vue
+++ src/views/index/components/footer.vue
@@ -21,19 +21,66 @@
         <p>Enjoy a Secure, Private Global Account, ECB-Protected Crypto Assets, with Waived Annual and Card Fees during the Trial Period</p>
       </div>
     </div>
-    <div class="footer-down">
+  </footer>
+
+  <footer class="view-pd h5-index-footer-container">
+    <div class="footer-up">
       <img
-        class="qrcode"
-        src="@/assets/images/p3.png"
+        src="@/assets/images/p2.png"
         alt=""
       >
-      <img
-        class="logo"
-        src="@/assets/images/p12.png"
-        alt=""
+      <p>Enjoy a Secure, Private Global Account, ECB-Protected Crypto Assets, with Waived Annual and Card Fees during the Trial Period</p>
+    </div>
+    <div class="footer-text">
+      <template
+        v-for="(item, index) in footerList"
+        :key="`h5-foot-column-${index}`"
       >
+        <div
+          class="column"
+          v-if="index !== 3"
+        >
+          <p
+            v-for="subItem in item.list"
+            :key="subItem"
+          >{{subItem}}</p>
+        </div>
+      </template>
+    </div>
+    <div
+      style="padding-top: .4rem;"
+      class="footer-text"
+    >
+      <template
+        v-for="(item, index) in footerList"
+        :key="`h5-foot-column-${index}`"
+      >
+        <div
+          style="padding: 0 .8rem; text-align: center;"
+          class="column"
+          v-if="index === 3"
+        >
+          <p
+            v-for="subItem in item.list"
+            :key="subItem"
+          >{{subItem}}</p>
+        </div>
+      </template>
     </div>
   </footer>
+
+  <div class="footer-down-code">
+    <img
+      class="qrcode"
+      src="@/assets/images/p3.png"
+      alt=""
+    >
+    <img
+      class="logo"
+      src="@/assets/images/p12.png"
+      alt=""
+    >
+  </div>
 </template>
 
 <script setup>
@@ -43,11 +90,6 @@ const activeNames = ref('')
 </script>
 
 <style lang="scss">
-@media only screen and (max-width: 750px) {
-  .index-footer-container {
-    display: none;
-  }
-}
 .index-footer-container {
   padding-top: 1.6rem;
   color: #fff;
@@ -97,52 +139,94 @@ const activeNames = ref('')
       opacity: 0.5;
     }
   }
+}
 
-  .footer-down {
-    padding-top: 1.28rem;
+.h5-index-footer-container {
+  padding-top: 1.2rem;
+
+  .footer-up {
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    .qrcode {
-      width: 1.424rem;
-      height: 1.376rem;
+    img {
+      width: 3.1rem;
+      height: 0.8rem;
     }
 
-    .logo {
-      margin-top: 0.24rem;
-      width: 2.192rem;
-      height: 0.664rem;
+    p {
+      padding: 0.4rem 1rem 0;
+      font-size: 12px;
+      color: #fff;
+      opacity: 0.8;
+      line-height: 1.2;
     }
   }
 
-  // @media only screen and (max-width: 750px) {
-  //   .footer-up {
-  //     display: block;
-  //   }
+  .footer-text {
+    padding-top: 1.2rem;
+    display: flex;
 
-  //   .footer-left {
-  //     p {
-  //       font-size: 12px;
+    .column {
+      flex: 1;
+    }
 
-  //       &:nth-child(1) {
-  //         font-size: 14px;
-  //       }
-  //     }
-  //   }
+    p {
+      padding: 0 20px 20px 0;
+      opacity: 0.8;
+      font-size: 14px;
+      color: #fff;
 
-  //   .footer-right {
-  //     padding: 20px 0 0 0;
-  //     align-items: center;
-  //     flex-direction: row;
-  //     width: auto;
+      &:nth-child(1) {
+        font-weight: bold;
+        opacity: 1;
+        font-size: 16px;
+      }
+    }
+  }
+}
 
-  //     p {
-  //       padding: 0 0 0 10px;
-  //       font-size: 12px;
-  //       line-height: 1.3;
-  //     }
-  //   }
-  // }
+.footer-down-code {
+  padding-top: .32rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .qrcode {
+    width: 1.424rem;
+    height: 1.376rem;
+  }
+
+  .logo {
+    margin-top: .16rem;
+    width: 2.192rem;
+    height: 0.664rem;
+  }
+}
+
+@media only screen and (min-width: 751px) {
+  .h5-index-footer-container {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 750px) {
+  .index-footer-container {
+    display: none;
+  }
+
+  .footer-down-code {
+    padding-top: .4rem;
+
+    .qrcode {
+      width: 1.68rem;
+      height: 1.64rem;
+    }
+    .logo {
+      margin-top: .4rem;
+      width: 3.14rem;
+      height: .96rem;
+    }
+  }
 }
 </style>
